9da8476e275e2342b40b91d4f832e88e417ae7f5
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-04-13 09:35:12 -0300

    style: minors changes

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-// ...código existente...
 const express = require('express');
 const cors = require('cors');
 const categoryRoutes = require('./src/routes/categoryRoutes');
 const itemRoutes = require('./src/routes/itemsRoutes');
 const userRoutes = require('./src/routes/userRoutes');
+const authRoutes = require('./src/routes/authRoutes')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, Refind!')
-})
-
 app.use('/items', itemRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/users', userRoutes);
+app.use('/auth', authRoutes)
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000')
